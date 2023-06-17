13024fb1939fc793ad30c6a119baa69e57daa675
Dmitriy <x5retail20@gmail.com>
2023-06-17 13:07:45 +0300

    адание 4

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        int tempNumber = number;
+
+        while (tempNumber != 0)
+        {
+            int digit = tempNumber % 10;
+            sum += digit;
+            tempNumber /= 10;
+        }
+
+        Console.WriteLine($"Сумма цифр числа: {sum}");
---
+++ Exercise3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+
+        Console.WriteLine("Введите 8 элементов массива:");
+
+        for (int i = 0; i < 8; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Массив: [");
+        for (int i = 0; i < 8; i++)
+        {
+            Console.Write(array[i]);
+
+            if (i != 7)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
